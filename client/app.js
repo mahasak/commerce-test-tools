0f40bb55d04a50be3a897949c5bdb7c4cd8c2f6c
Mahasak Pijittum <mahasak@gmail.com>
2019-04-29 10:55:57 +0800

    add test client

---
+++ client/app.js
@@ -0,0 +1,41 @@
+
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express().use(bodyParser.json()); // creates http server
+const token = 'test'; // type here your verification token
+
+app.get('/', (req, res) => {
+    // check if verification token is correct
+    if (req.query.token !== token) {
+        return res.sendStatus(401);
+    }
+
+    // return challenge
+    return res.end(req.query.challenge);
+});
+
+app.post('/webhook', (req, res) => {
+    // check if verification token is correct
+    if (req.query.token !== token) {
+        return res.sendStatus(401);
+    }
+  
+    // print request body
+    console.log(req.body);
+
+    // return a text response
+    const data = {
+        responses: [
+            {
+                type: 'text',
+                elements: ['Hi', 'Hello']
+            }
+        ]
+    };
+
+    res.json(data);
+});
+
+app.listen(3000, () => console.log('[Commerce-Webhook] is listening'));
